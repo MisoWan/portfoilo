a3fa6e4bdac46dcdff39594584835ed01a6fe0a3
MisoWan <jopd4@naver.com>
2022-12-11 00:01:06 -0500

    JS : Home transparent

--- main.js
+++ main.js
@@ -28,6 +28,17 @@ homeContactBtn.addEventListener('click', ()=>{
     scrollIntoView('#contact')
 });
 
+// Make home slowly fade to trasparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+       home.style.opacity = 1-window.scrollY/homeHeight;
+})
+
+
+
+
+
 function scrollIntoView(selector){
     const contact = document.querySelector(selector);
     contact.scrollIntoView({behavior : 'smooth'})
